aed2cd9671f5c44d747a851abb9b393609a0569e
Rustam Shangareev <rustam.letitbe@gmail.com>
2022-12-14 23:42:40 +0300

    Update main_summarizer.py
    Eliminate global variables, move functionality to the class, fix summarization function

--- main_summarizer.py
+++ main_summarizer.py
@@ -1,20 +1,20 @@
 from transformers import pipeline
 
-MAX_TOKEN_COUNT = 700
+MAX_TOKEN_COUNT = 1024
 TEXT_LENGTH_INTERVAL = 100
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 
-def summarize(text, min_size):
-	size = len(text.split())
-	strip_len = (size - MAX_TOKEN_COUNT + 1) // 2
-	stripped_words = text.split()[strip_len:-strip_len]
+class Summarizer:
+	pipe = pipeline("summarization", model="facebook/bart-large-cnn")
 
-	stripped_text = ""
-	for word in stripped_words:
-		stripped_text += word + ' '
-	return str(
-		summarizer(
-			stripped_text, min_length=min_size, max_length=(min_size + TEXT_LENGTH_INTERVAL), do_sample=False
-		)[0]['summary_text']
-	)
+	def summarize(self, text, min_size):
+		trimmed_text = text
+
+		while len(self.pipe.tokenizer(trimmed_text).tokens()) > MAX_TOKEN_COUNT:
+			trimmed_text = trimmed_text[TEXT_LENGTH_INTERVAL:-TEXT_LENGTH_INTERVAL]
+
+		return str(
+			self.pipe(
+				trimmed_text, min_length=min_size, max_length=(min_size + TEXT_LENGTH_INTERVAL), do_sample=False
+			)[0]['summary_text']
+		)
