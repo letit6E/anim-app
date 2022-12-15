dbc8b604592cad843bcc6df704685190ffffd2cd
Rustam Shangareev <rustam.letitbe@gmail.com>
2022-12-15 23:53:33 +0300

    Update main_summarizer.py
    Move constants to class, make __init__ function of class

--- main_summarizer.py
+++ main_summarizer.py
@@ -1,20 +1,21 @@
 from transformers import pipeline
 
-MAX_TOKEN_COUNT = 1024
-TEXT_LENGTH_INTERVAL = 100
-
 
 class Summarizer:
-	pipe = pipeline("summarization", model="facebook/bart-large-cnn")
+	MAX_TOKEN_COUNT = 1024
+	TEXT_LENGTH_INTERVAL = 100
+
+	def __init__(self):
+		self.pipe = pipeline("summarization", model="facebook/bart-large-cnn")
 
 	def summarize(self, text, min_size):
 		trimmed_text = text
 
-		while len(self.pipe.tokenizer(trimmed_text).tokens()) > MAX_TOKEN_COUNT:
-			trimmed_text = trimmed_text[TEXT_LENGTH_INTERVAL:-TEXT_LENGTH_INTERVAL]
+		while len(self.pipe.tokenizer(trimmed_text).tokens()) > self.MAX_TOKEN_COUNT:
+			trimmed_text = trimmed_text[self.TEXT_LENGTH_INTERVAL:-self.TEXT_LENGTH_INTERVAL]
 
 		return str(
 			self.pipe(
-				trimmed_text, min_length=min_size, max_length=(min_size + TEXT_LENGTH_INTERVAL), do_sample=False
+				trimmed_text, min_length=min_size, max_length=(min_size + self.TEXT_LENGTH_INTERVAL), do_sample=False
 			)[0]['summary_text']
 		)
